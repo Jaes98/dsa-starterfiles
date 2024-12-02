6c20723a1c9d635e872c45334c2055cd19491747
Jaes98 <marcus.jaes@gmail.com>
2024-12-02 13:36:43 +0100

    commit

---
+++ GridRunner/Calculate.java
@@ -0,0 +1,30 @@
+public class Calculate {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+        int c = 40;
+
+
+        int r = a + b * c;
+        System.out.println("Result is: " + r);
+    }
+    
+}
+
+
+
+
+
+
+// 85 enter, 31 enter, -
+
+// 3 enter, 4 enter, 5 enter, 2 enter, - * +
+
+// 12 enter, 45 enter, kvadratrot enter, * 
+
+// 5 enter, 9 enter, 4 enter, dividere
+
+// 12 enter, 3 enter, +, 7 enter, 9 enter, +, *
+
+// 23 enter, 23 i 2., 13 enter, 9 enter, *, -, 5 enter, 7 enter, dividere, +
+
